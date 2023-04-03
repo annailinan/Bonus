43c656161ed93f8c338bdf596666369cfd216ad6
Anna Li <Kristusha3@yandex.ru>
2023-04-03 22:29:24 +0300

    commit3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int summacheck = 200; // сумма на счету
-        int replenishment = 100; //сумма пополнения счета
-        int bonus;
-        int totlBonus;
+        int replenishment = 5000; //сумма пополнения счета
         if (replenishment > 1000) {
             System.out.println(" Начислено бонусов " + replenishment / 100);
             System.out.println(" Ваш баланс с учетом бонусов " + (summacheck + replenishment + replenishment / 100));
